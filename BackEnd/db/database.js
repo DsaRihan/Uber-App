a354805f531ca19b38bb3466df7998dba5088b73
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-08 00:13:02 +0530

    modified

--- BackEnd/db/database.js
+++ BackEnd/db/database.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT).then(
-        ()=>{console.log("connected To Db")}
-    );
+    try {
+        mongoose.connect(process.env.DB_URL, {
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
 }
 
 module.exports = connectToDb;
